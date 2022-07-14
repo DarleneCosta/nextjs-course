5f9b87daa6ad3d0f9abdf35fd0649d89472e09c5
darlenecosta <darl_mel@hotmail.com>
2022-07-14 09:44:57 -0300

    componentes layout

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,3 @@
-import React from "react";
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-function Title({ children, as }) {
-  const Tag = as;
-  return (
-    <React.Fragment>
-      {" "}
-      <Tag>{children}</Tag>
-      <style jsx>{`
-        ${Tag} {
-          color: blue;
-        }
-      `}</style>
-    </React.Fragment>
-  );
-}
-export default function HomePage() {
-  return (
-    <div>
-      <GlobalStyle />
-      <Title as="h1"> Alura Cases - Home Page</Title>
-      <Link href="/faq" passHref>
-        Ir para o FAQ
-      </Link>
-    </div>
-  );
-}
+export default HomeScreen;
